019dd861ad288db03605fab69c16becfa92638da
Nasir Islam <nasir.islam1983@hotmail.com>
2023-12-29 15:35:57 +0400

    Mongo db changes

---
+++ src/services/catalog/catalog.api/Entities/Products.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+namespace catalog.api.Entities
+
+{
+    public class Products
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        private string id;
+        public string Id { get=>id; set=>id; }
+        [BsonElement("Name")]
+        public string? Name { get; set; }
+
+        public string? Category { get; set; }
+        public string? Summary { get; set; }
+        public string? Description { get; set; }
+        public string? ImageFile { get; set; }
+        public string? Price { get; set; }
+
+        
+    }
+}
