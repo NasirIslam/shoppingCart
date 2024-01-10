7c48143c5ec85092733ed331605c5750a8f2aad6
NasirIslam <nasir.islam1983@gmail.com>
2024-01-10 20:55:17 +0400

    test

--- src/services/basket/basket.api/Entities/ShopptingCartItems.cs
+++ src/services/basket/basket.api/Entities/ShopptingCartItems.cs
@@ -10,4 +10,5 @@
         public string ProductName { get; set; }
 
     }
+                                                                                                                                                                              32
 }
